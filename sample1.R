02bf0a9e35e201f39a4cbe28869947f9ee2b6d6e
tonym777 <48734924+tonym777@users.noreply.github.com>
2020-04-14 19:14:47 -0400

    Update sample1.R
    add implied vol

--- sample1.R
+++ sample1.R
@@ -1,9 +1,24 @@
-# demostrate cor and cov function within R
+# demostrate cor, cov, implied vol function within R
 
 df <- read.table("ab.csv", header=TRUE, sep=",")
 a <- df[2]
 b <- df[3]
+r <- cor(a, b, use="complete.obs", method="pearson") 
+v <- cov(a, b, use="complete.obs")
 
 
-cor(a, b, use="complete.obs", method="pearson") 
-cov(a, b, use="complete.obs")
+myImpVol <- function(S, K, T, r, q, sigma){
+  #S: spot price
+  #K: strike price
+  #T: time to maturity
+  #r: interest rate
+  #sigma: volatility of underlying asset
+  #q: continuous dividend rate
+  
+  d1 <- (log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * sqrt(T))
+  
+  vega <- 1 / sqrt(2 * pi) * S * exp(-q * T) * exp(-(d1 ** 2) * 0.5) * sqrt(T)
+  
+  return(vega)
+
+}
