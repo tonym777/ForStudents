ccede303de865c402f7e3a48b18ff446dbadb0b5
tonym777 <48734924+tonym777@users.noreply.github.com>
2020-04-15 14:46:02 -0400

    Update sample1.R
    tide up

--- sample1.R
+++ sample1.R
@@ -6,7 +6,10 @@ b <- df[3]
 r <- cor(a, b, use="complete.obs", method="pearson") 
 v <- cov(a, b, use="complete.obs")
 
-
+####
+####  BS option model
+####
+####
 myImpVol <- function(S, K, T, r, q, sigma){
   #S: spot price
   #K: strike price
@@ -16,21 +19,19 @@ myImpVol <- function(S, K, T, r, q, sigma){
   #q: continuous dividend rate
   
   d1 <- (log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * sqrt(T))
-  d2 <- d1 - sigma * sqrt(t)
+  d2 <- d1 - sigma * sqrt(T)
   vega <- 1 / sqrt(2 * pi) * S * exp(-q * T) * exp(-(d1 ** 2) * 0.5) * sqrt(T)
-  
-  result <- list(d1, d2, vega)
-  return(result)
+  return (c(d1, d2, vega))
 }
 
 callOption <- function(S, K, T, r, q, sigma)  {
-  result <- myImpVol(S, K, T, r, q, sigma)
-  c <- pnorm(result$d1) * S - pnorm(result$d2) * K * exp(-r * t)
+  list(d1,d2,vega) <- myImpVol(S, K, T, r, q, sigma)
+  c <- pnorm(d1) * S - pnorm(d2) * K * exp(-r * T)
   return(c)
 }
 
 putOption <- function(S, K, T, r, q, sigma)  {
-  result <- myImpVol(S, K, T, r, q, sigma)
-  p <- pnorm(-result$d2) * K * exp(-r * t) - pnorm(-result$d1) * S
+  list(d1,d2,vega) <- myImpVol(S, K, T, r, q, sigma)
+  p <- pnorm(-d2) * K * exp(-r * T) - pnorm(-d1) * S
   return(p)
 }
