ca619eb5a5803d570e7891eccf435f28fb851d2c
tonym777 <48734924+tonym777@users.noreply.github.com>
2020-04-15 00:39:24 -0400

    Update sample1.R

--- sample1.R
+++ sample1.R
@@ -16,9 +16,21 @@ myImpVol <- function(S, K, T, r, q, sigma){
   #q: continuous dividend rate
   
   d1 <- (log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * sqrt(T))
-  
+  d2 <- d1 - sigma * sqrt(t)
   vega <- 1 / sqrt(2 * pi) * S * exp(-q * T) * exp(-(d1 ** 2) * 0.5) * sqrt(T)
   
-  return(vega)
+  result <- list(d1, d2, vega)
+  return(result)
+}
 
+callOption <- function(S, K, T, r, q, sigma)  {
+  result <- myImpVol(S, K, T, r, q, sigma)
+  c <- pnorm(result$d1) * S - pnorm(result$d2) * K * exp(-r * t)
+  return(c)
+}
+
+putOption <- function(S, K, T, r, q, sigma)  {
+  result <- myImpVol(S, K, T, r, q, sigma)
+  p <- pnorm(-result$d2) * K * exp(-r * t) - pnorm(-result$d1) * S
+  return(p)
 }
